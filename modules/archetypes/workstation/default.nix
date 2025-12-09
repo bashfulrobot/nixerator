b8a58ced97839480e839058150734eebd630aecf
Dustin Krysak <dustin@bashfulrobot.com>
2025-12-09 14:22:20 -0800

    feat(workstation): ✨ create workstation archetype
    - Add workstation archetype for desktop systems
    - Enables browsers suite with Google Chrome
    - Provides foundation for common workstation configuration

---
+++ modules/archetypes/workstation/default.nix
@@ -0,0 +1,19 @@
+{ lib, config, ... }:
+
+let
+  cfg = config.archetypes.workstation;
+in
+{
+  options = {
+    archetypes.workstation.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable workstation archetype with browser suite and common workstation tools.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Enable browser suite
+    suites.browsers.enable = true;
+  };
+}
