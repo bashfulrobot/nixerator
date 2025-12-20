06857602eceb6a20a1f680244d54e70b8e003441
Dustin Krysak <dustin@bashfulrobot.com>
2025-12-19 20:40:05 -0600

    refactor: remove hyprshell flake input (now in nixpkgs)

--- flake.nix
+++ flake.nix
@@ -33,22 +33,12 @@
         stylix.follows = "stylix";
         hyprland.follows = "hyprland";
         waybar-auto-hide.follows = "waybar-auto-hide";
-        hyprshell.follows = "hyprshell";
       };
     };
 
     waybar-auto-hide = {
       url = "github:bashfulrobot/nixpkg-waybar-auto-hide";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprshell = {
-      url = "github:H3rmt/hyprshell/hyprshell";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-        hyprland.follows = "hyprland";
-      };
     };
 
     llm-agents = {
