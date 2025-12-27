48c0c60f5e38777fb984c05948f23139d9b16e69
Dustin Krysak <dustin@bashfulrobot.com>
2025-12-26 16:57:36 -0800

    feat(nix): enable zoxide for smarter directory navigation in CLI

---
+++ modules/apps/cli/zoxide/default.nix
@@ -0,0 +1,39 @@
+{
+  globals,
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+
+let
+  cfg = config.apps.cli.zoxide;
+  username = globals.user.name;
+in
+{
+  options = {
+    apps.cli.zoxide.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable zoxide for smarter directory navigation.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    # Home Manager user configuration
+    home-manager.users.${username} = {
+
+      programs.zoxide = {
+        enable = true;
+        enableFishIntegration = true;
+        options = [
+          # Replace cd.
+          "--cmd cd"
+        ];
+      };
+
+    };
+
+  };
+}
