f23795b6c17e438ec92e38a7e0dfc015b151912b
Dustin Krysak <dustin@bashfulrobot.com>
2026-01-16 14:17:59 -0800

    feat(helium): 🔒 add 1Password browser integration
    - Add custom_allowed_browsers entry for Helium
    - Configure native messaging host for 1Password extension
    - Enable browser extension unlock via desktop app

--- modules/apps/gui/helium/default.nix
+++ modules/apps/gui/helium/default.nix
@@ -8,11 +8,11 @@
 # Current local version: 0.8.1 (see ../../packages/helium/default.nix)
 # Note: Helium is currently in beta
 
-{ lib, pkgs, config, ... }:
+{ lib, pkgs, config, globals, ... }:
 
 let
   cfg = config.apps.gui.helium;
-
+  username = globals.user.name;
 in
 {
   options = {
@@ -29,5 +29,29 @@ in
       # See ../../packages/helium/default.nix for version details
       helium
     ];
+
+    # 1Password browser integration
+    environment.etc."1password/custom_allowed_browsers".text = lib.mkAfter ''
+      helium
+    '';
+
+    # Home manager configuration
+    home-manager.users.${username} = {
+      # 1Password native messaging host for Helium
+      home.file.".config/net.imput.helium/NativeMessagingHosts/com.1password.1password.json".text = builtins.toJSON {
+        name = "com.1password.1password";
+        description = "1Password BrowserSupport";
+        path = "/run/wrappers/bin/1Password-BrowserSupport";
+        type = "stdio";
+        allowed_origins = [
+          "chrome-extension://hjlinigoblmkhjejkmbegnoaljkphmgo/"
+          "chrome-extension://bkpbhnjcbehoklfkljkkbbmipaphipgl/"
+          "chrome-extension://gejiddohjgogedgjnonbofjigllpkmbf/"
+          "chrome-extension://khgocmkkpikpnmmkgmdnfckapcdkgfaf/"
+          "chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa/"
+          "chrome-extension://dppgmdbiimibapkepcbdbmkaabgiofem/"
+        ];
+      };
+    };
   };
 }
