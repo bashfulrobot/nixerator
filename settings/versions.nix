48c400fad2197f08bf1800bdcfd5f98ec8c6d485
Dustin Krysak <dustin@bashfulrobot.com>
2026-02-05 10:51:24 -0800

    chore(kiyoproctrls): 🔧 remove module and version pin

--- settings/versions.nix
+++ settings/versions.nix
@@ -10,14 +10,6 @@
       sha256 = "sha256-Oyqj4DHUOyxxvNVDtDKQSagX48k87o9p9baqLxBiJec=";
       repo = "https://github.com/bashfulrobot/meetsum";
     };
-
-    kiyoproctrls = {
-      # Source: https://github.com/soyersoyer/kiyoproctrls
-      version = "unstable-2022-06-08";
-      rev = "1aeb0226ab416b592ee39e0ecc8ccacd2e7e0efc";
-      sha256 = "sha256-Y+lrWaWEWmpkSAOWJMquUM/XuiAjZvz3XW94iPSy66U=";
-      repo = "https://github.com/soyersoyer/kiyoproctrls";
-    };
   };
 
   # Add future version pins here organized by category:
