bd51da5c9d69edcf8fb817d4e4982e26a7b35877
Dustin Krysak <dustin@bashfulrobot.com>
2026-02-14 08:08:19 -0800

    feat(cachix): ✨ add numtide binary cache and simplify config

--- modules/system/cachix/default.nix
+++ modules/system/cachix/default.nix
@@ -1,26 +1,15 @@
-{ config, lib, ... }:
-
-# Hyprland Binary Cache Configuration
-# Enables cachix.org binary cache for Hyprland to avoid building from source
+{ ... }:
 
 {
-  options.system.cachix = {
-    hyprland = {
-      enable = lib.mkEnableOption "Hyprland binary cache" // {
-        default = true;
-      };
-    };
-  };
+  nix.settings = {
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://cache.numtide.com"
+    ];
 
-  config = lib.mkIf config.system.cachix.hyprland.enable {
-    nix.settings = {
-      substituters = [
-        "https://hyprland.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
-    };
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
   };
 }
