2cee36021ef4dc5ddfe6c5351b224fde855b987e
Dustin Krysak <dustin@bashfulrobot.com>
2026-02-23 17:20:07 -0800

    refactor(nix): ♻️ nest nix settings into single attribute block

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -1,17 +1,20 @@
 _:
 
 {
-  nix.nixPath = [ ];
 
-  nix.settings = {
-    substituters = [
-      "https://hyprland.cachix.org"
-      "https://cache.numtide.com"
-    ];
+  nix = {
+    nixPath = [ ];
 
-    trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-    ];
+    settings = {
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://cache.numtide.com"
+      ];
+
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      ];
+    };
   };
 }
