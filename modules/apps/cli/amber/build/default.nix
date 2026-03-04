b3672e0da952f1f3ca412cf9aa3ede2060b5b46d
Dustin Krysak <dustin@bashfulrobot.com>
2026-03-04 15:39:15 -0800

    feat(amber): ✨ add amber code search and replace tool
    via [Happy](https://happy.engineering)

---
+++ modules/apps/cli/amber/build/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  pkgs,
+  versions,
+  ...
+}:
+pkgs.stdenv.mkDerivation {
+  name = "amber";
+  src = pkgs.fetchurl {
+    url = "https://github.com/dalance/amber/releases/download/v${versions.cli.amber.version}/amber-v${versions.cli.amber.version}-x86_64-lnx.zip";
+    inherit (versions.cli.amber) sha256;
+  };
+
+  nativeBuildInputs = [ pkgs.unzip ];
+
+  dontBuild = true;
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ambs $out/bin/ambs
+    cp ambr $out/bin/ambr
+    chmod +x $out/bin/ambs $out/bin/ambr
+  '';
+
+  meta = with lib; {
+    description = "Code search and replace tool providing ambs (search) and ambr (replace) - https://github.com/dalance/amber";
+    maintainers = [ ];
+  };
+}
