8765fcb027644da0fbc1b0885acbed450c8cc695
Dustin Krysak <dustin@bashfulrobot.com>
2026-03-06 12:40:14 -0800

    feat(special-workspaces): ✨ add music and dev special workspaces
    - Add SUPER+M and SUPER+D bindings for music and dev workspaces
    - Add SUPER+SHIFT+M/D to move windows to new workspaces
    - Update module description to include music and dev categories

--- modules/system/special-workspaces/default.nix
+++ modules/system/special-workspaces/default.nix
@@ -9,19 +9,23 @@ let
   cfg = config.system.special-workspaces;
 in
 {
-  options.system.special-workspaces.enable = lib.mkEnableOption "Hyprland special workspaces for task and office apps";
+  options.system.special-workspaces.enable = lib.mkEnableOption "Hyprland special workspaces for task, office, music, and dev apps";
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${globals.user.name} = {
       xdg.configFile."hypr/conf.d/special-workspaces.conf".text = ''
         # Special Workspaces: named special workspaces toggled by shortcut
-        # SUPER+W = Work (task manager), SUPER+O = Office (comms/calendar)
+        # SUPER+W = Work, SUPER+O = Office, SUPER+M = Music, SUPER+D = Dev
 
         # Keybinds: toggle and move-to
         bind = SUPER, W, togglespecialworkspace, work
         bind = SUPER SHIFT, W, movetoworkspace, special:work
         bind = SUPER, O, togglespecialworkspace, office
         bind = SUPER SHIFT, O, movetoworkspace, special:office
+        bind = SUPER, M, togglespecialworkspace, music
+        bind = SUPER SHIFT, M, movetoworkspace, special:music
+        bind = SUPER, D, togglespecialworkspace, dev
+        bind = SUPER SHIFT, D, movetoworkspace, special:dev
 
         # Window rules: assign apps to their special workspace on launch
         windowrule {
