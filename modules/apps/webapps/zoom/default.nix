afe2cbc8251a0ac0aff0448526d0f6c3e9c07337
Dustin Krysak <dustin@bashfulrobot.com>
2026-03-18 16:05:57 -0700

    feat(webapps): ✨ add zoom web app and append %u to exec lines

---
+++ modules/apps/webapps/zoom/default.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  config,
+  globals,
+  ...
+}:
+let
+  mkWebApp = import ../../../../lib/mkWebApp.nix { inherit lib; };
+in
+mkWebApp {
+  inherit config globals;
+  name = "zoom";
+  displayName = "Zoom";
+  url = "https://app.zoom.us/wc/home";
+  wmClass = "chrome-app.zoom.us__wc_home-Default";
+  icon = ./icon.png;
+  categories = [
+    "Network"
+    "VideoConference"
+  ];
+  mimeTypes = [ "x-scheme-handler/zoommtg" ];
+  defaultFor = {
+    "x-scheme-handler/zoommtg" = [ "zoom-webapp.desktop" ];
+  };
+}
