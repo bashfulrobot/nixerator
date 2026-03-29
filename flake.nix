d42c0b2b974c40e697c2bbee1cbc269fa15dfcad
Dustin Krysak <dustin@bashfulrobot.com>
2026-03-29 10:44:18 -0600

    deps(upsight): ⬆️ bump upsight to v0.2.0

--- flake.nix
+++ flake.nix
@@ -78,7 +78,7 @@
     # };
 
     upsight = {
-      url = "github:bashfulrobot/upsight/v0.1.2";
+      url = "github:bashfulrobot/upsight/v0.2.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
