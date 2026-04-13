1033ac73404a7055c254f298302d12795030ce1f
Dustin Krysak <dustin@bashfulrobot.com>
2026-04-13 09:18:22 -0700

    feat(webapps): ✨ add Clay desktop webapp to AI suite

---
+++ modules/apps/webapps/clay/default.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  config,
+  globals,
+  ...
+}:
+let
+  mkWebApp = import ../../../../lib/mkWebApp.nix { inherit lib; };
+in
+mkWebApp {
+  inherit config globals;
+  name = "clay";
+  displayName = "Clay";
+  url = "https://192.168.169.2:3131/";
+  wmClass = "chrome-192.168.169.2__3131-Default";
+  icon = ./icon.png;
+}
